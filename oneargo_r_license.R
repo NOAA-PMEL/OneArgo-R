bd0e53a6951a489a80c3679583188a93089c57c4
mcornec <marin.cornec@noaa.gov>
2022-05-31 17:56:29 -0700

    Update and rename bgc_argo_workshop_R_license.R to oneargo_r_license.R

--- oneargo_r_license.R
+++ oneargo_r_license.R
@@ -1,3 +1,19 @@
+# LEGAL DISCLAIMER
+#
+# This toolbox is a software product and is not official
+# communication of the National Oceanic and Atmospheric Administration
+# (NOAA), or the United States Department of Commerce (DOC). All NOAA
+# project code is provided on an 'as is' basis and the user
+# assumes responsibility for its use. Any claims against the DOC or DOC
+# bureaus stemming from the use of this project will be governed
+# by all applicable Federal law. Any reference to specific commercial
+# products, processes, or services by service mark, trademark,
+# manufacturer, or otherwise, does not constitute or imply their
+# endorsement, recommendation, or favoring by the DOC. The DOC seal and
+# logo, or the seal and logo of a DOC bureau, shall not be used in any
+# manner to imply endorsement of any commercial product or activity by
+# the DOC or the United States Government.
+#
 #  LICENSE
 # 
 #  This toolbox is governed by the GNU General Public License, version 3 (GPLv3):
